eaa426e458024d088d36af342e23ef7a59036379
け <challengerk.8000@gmail.com>
2023-02-10 22:05:07 +0900

    add: FollowsFuncions

---
+++ src/PheasantTails.TwiHigh.Model/Followers/AddNewFolloweeTweetContext.cs
@@ -0,0 +1,8 @@
+﻿namespace PheasantTails.TwiHigh.Model.Followers
+{
+    public class AddNewFolloweeTweetContext
+    {
+        public Guid FolloweeId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
