a119049fb16797e1100a9c69b09daa761e8dbe54
け <challengerk.8000@gmail.com>
2023-02-13 20:49:20 +0900

    feat: Added feature of getting my timelines.

---
+++ src/PheasantTails.TwiHigh.Model/Timelines/ResponseTimelineContext.cs
@@ -0,0 +1,11 @@
+﻿using PheasantTails.TwiHigh.DataStore.Entity;
+
+namespace PheasantTails.TwiHigh.Model.Timelines
+{
+    public class ResponseTimelineContext
+    {
+        public DateTimeOffset Latest { get; set; }
+        public DateTimeOffset Oldest { get; set; }
+        public Tweet[] Tweets { get; set; } = Array.Empty<Tweet>();
+    }
+}
