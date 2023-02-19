bd024eabe87f84486f10c45a5e6a4d3b73ba2d5e
け <challengerk.8000@gmail.com>
2023-02-20 01:03:00 +0900

    add: Added client side ui project.

---
+++ src/PheasantTails.TwiHigh.Client/Components/TweetComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using PheasantTails.TwiHigh.DataStore.Entity;
+
+namespace PheasantTails.TwiHigh.Client.Components
+{
+    public partial class TweetComponent
+    {
+        [Parameter]
+        public Tweet? Tweet { get; set; }
+    }
+}
