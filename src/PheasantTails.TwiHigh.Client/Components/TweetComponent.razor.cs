cb1b157c0dd120788ce54e4ceb7d718484e3816c
け <challengerk.8000@gmail.com>
2023-02-24 22:34:01 +0900

    update: Changed datetime strings style in TweetComponent

--- src/PheasantTails.TwiHigh.Client/Components/TweetComponent.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Components/TweetComponent.razor.cs
@@ -7,5 +7,37 @@ namespace PheasantTails.TwiHigh.Client.Components
     {
         [Parameter]
         public Tweet? Tweet { get; set; }
+
+        private string CreateAt
+        {
+            get
+            {
+                if (Tweet == null)
+                {
+                    return string.Empty;
+                }
+
+                // 1年前ならyyyy/mm/dd
+                if (Tweet.CreateAt <= DateTimeOffset.UtcNow.AddYears(-1))
+                {
+                    return Tweet.CreateAt.ToLocalTime().ToString("yyyy/MM/dd");
+                }
+
+                // 24時間より前ならm/d
+                if (Tweet.CreateAt <= DateTimeOffset.UtcNow.AddDays(-1))
+                {
+                    return Tweet.CreateAt.ToLocalTime().ToString("M/d");
+                }
+
+                // 1時間より前なら H:mm
+                if (Tweet.CreateAt <= DateTimeOffset.UtcNow.AddHours(-1))
+                {
+                    return Tweet.CreateAt.ToLocalTime().ToString("H:mm");
+                }
+
+                // H:mm:ss
+                return Tweet.CreateAt.ToLocalTime().ToString("H:mm:ss");
+            }
+        }
     }
 }
