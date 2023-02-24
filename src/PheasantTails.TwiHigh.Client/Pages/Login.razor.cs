459b24d7d8d6433eb2a4ec7105fc5b0af880a8c8
け <challengerk.8000@gmail.com>
2023-02-25 01:07:34 +0900

    Add: AppUserHttpClient added.

--- src/PheasantTails.TwiHigh.Client/Pages/Login.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Pages/Login.razor.cs
@@ -1,6 +1,15 @@
-﻿namespace PheasantTails.TwiHigh.Client.Pages
+﻿using Microsoft.AspNetCore.Components;
+using PheasantTails.TwiHigh.Client.TypedHttpClients;
+
+namespace PheasantTails.TwiHigh.Client.Pages
 {
     public partial class Login
     {
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private AppUserHttpClient AppUserHttpClient { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+
     }
 }
