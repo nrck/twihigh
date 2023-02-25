035024e87648871bd192ea0a52c9ab6f50cf880b
け <challengerk.8000@gmail.com>
2023-02-25 23:37:34 +0900

    update: Created design on Login.razor.

--- src/PheasantTails.TwiHigh.Client/Pages/Login.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Pages/Login.razor.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using PheasantTails.TwiHigh.Client.TypedHttpClients;
+using PheasantTails.TwiHigh.Model.TwiHighUsers;
 
 namespace PheasantTails.TwiHigh.Client.Pages
 {
@@ -10,6 +12,41 @@ namespace PheasantTails.TwiHigh.Client.Pages
         private AppUserHttpClient AppUserHttpClient { get; set; }
 #pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
 
+        private PostAuthorizationContext PostAuthorizationContext { get; set; } = new PostAuthorizationContext();
 
+        private string ErrorMessage { get; set; } = string.Empty;
+
+        private bool IsLoginWorking { get; set; } = false;
+
+        private async Task OnClickLoginButtonAsync(MouseEventArgs _)
+        {
+            ErrorMessage = string.Empty;
+            if (IsLoginWorking == true)
+            {
+                return;
+            }
+
+            IsLoginWorking = true;
+            if (string.IsNullOrEmpty(PostAuthorizationContext.DisplayId))
+            {
+                ErrorMessage = "ユーザ名を入力してください。";
+                IsLoginWorking = false;
+                return;
+            }
+            if (string.IsNullOrEmpty(PostAuthorizationContext.PlanePassword))
+            {
+                ErrorMessage = "パスワードを入力してください。";
+                IsLoginWorking = false;
+                return;
+            }
+
+            var res = await AppUserHttpClient.LoginAsync(PostAuthorizationContext);
+            if (string.IsNullOrEmpty(res?.Token))
+            {
+                ErrorMessage = "ログインできませんでした。ユーザ名とパスワードを確認してください。";
+            }
+            IsLoginWorking = false;
+            return;
+        }
     }
 }
