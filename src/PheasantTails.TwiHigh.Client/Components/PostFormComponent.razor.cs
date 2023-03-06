8e182df7c47cf558faddc5cd0d703a67c6d8adf0
け <challengerk.8000@gmail.com>
2023-03-06 23:55:55 +0900

    Update: Added feature of post a tweet.

--- src/PheasantTails.TwiHigh.Client/Components/PostFormComponent.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Components/PostFormComponent.razor.cs
@@ -1,24 +1,40 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using PheasantTails.TwiHigh.Client.TypedHttpClients;
 using PheasantTails.TwiHigh.Model;
 
 namespace PheasantTails.TwiHigh.Client.Components
 {
     public partial class PostFormComponent
     {
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private TweetHttpClient TweetHttpClient { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private ILocalStorageService LocalStorage { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
         [Parameter]
         public string UserAvatarUrl { get; set; } = string.Empty;
 
-        public PostTweetContext PostTweetContext { get; set; }
+        public PostTweetContext PostTweetContext { get; set; } = new PostTweetContext();
 
-        protected override Task OnInitializedAsync()
+        protected override async Task OnInitializedAsync()
         {
-            PostTweetContext = new PostTweetContext();
-            return base.OnInitializedAsync();
+            var token = await LocalStorage.GetItemAsStringAsync("TwiHighJwt");
+            TweetHttpClient.SetToken(token);
+            await base.OnInitializedAsync();
         }
 
-        private void OnSubmit()
+        private async Task OnSubmitAsync()
         {
+            await TweetHttpClient.PostTweetAsync(PostTweetContext);
             Console.WriteLine("OnSubmit:" + PostTweetContext.Text);
+            PostTweetContext.Text = string.Empty;
+            PostTweetContext.ReplyTo = null;
         }
     }
 }
