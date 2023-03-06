3c105d69ecbd62e8ed7b69b1a8083a1d20602c25
け <challengerk.8000@gmail.com>
2023-03-06 23:55:55 +0900

    Update: Added feature of post a tweet.

---
+++ src/PheasantTails.TwiHigh.Client/TypedHttpClients/TweetHttpClient.cs
@@ -0,0 +1,39 @@
+﻿using PheasantTails.TwiHigh.Model;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace PheasantTails.TwiHigh.Client.TypedHttpClients
+{
+    public class TweetHttpClient
+    {
+        private const string API_URL_TWEET = "http://localhost:5003/api/Tweets";
+        private readonly HttpClient _httpClient;
+
+        public TweetHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public void SetToken(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        public async Task<HttpResponseMessage?> PostTweetAsync(PostTweetContext context)
+        {
+            try
+            {
+                return await _httpClient.PostAsJsonAsync(API_URL_TWEET, context);
+            }
+            catch (Exception)
+            {
+                return await Task.FromResult<HttpResponseMessage?>(default);
+            }
+        }
+    }
+}
