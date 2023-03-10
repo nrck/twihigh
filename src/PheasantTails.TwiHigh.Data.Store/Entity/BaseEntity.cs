85ef2e320ee448bfa550e6fc09ad09d8828f1d37
け <challengerk.8000@gmail.com>
2023-03-10 19:36:34 +0900

    change: Entities changed to use base entity.

---
+++ src/PheasantTails.TwiHigh.Data.Store/Entity/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace PheasantTails.TwiHigh.Data.Store.Entity
+{
+    public abstract class BaseEntity
+    {
+
+        public virtual Guid Id { get; set; } = Guid.Empty;
+        public virtual DateTimeOffset CreateAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
--- src/PheasantTails.TwiHigh.Data.Store/Entity/TwiHighUser.cs
+++ src/PheasantTails.TwiHigh.Data.Store/Entity/TwiHighUser.cs
@@ -1,16 +1,14 @@
 ﻿namespace PheasantTails.TwiHigh.Data.Store.Entity
 {
-    public class TwiHighUser
+    public class TwiHighUser : BaseEntity
     {
         public const string PARTITION_KEY = "/id";
 
-        public Guid Id { get; set; }
         public string DisplayId { get; set; } = string.Empty;
         public string DisplayName { get; set; } = string.Empty;
         public string HashedPassword { get; set; } = string.Empty;
         public string Biography { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public DateTimeOffset CreateAt { get; set; } = DateTimeOffset.UtcNow;
         public long Tweets { get; set; }
         public string AvatarUrl { get; set; } = string.Empty;
         public Guid[] Follows { get; set; } = Array.Empty<Guid>();
