8905336f276d7fdfe86555d13ce75123ae3fd956
け <challengerk.8000@gmail.com>
2023-03-13 00:38:29 +0900

    update: Updated to feature of sign up.

--- src/PheasantTails.TwiHigh.Data.Model/TwiHighUsers/AddTwiHighUserContext.cs
+++ src/PheasantTails.TwiHigh.Data.Model/TwiHighUsers/AddTwiHighUserContext.cs
@@ -1,4 +1,6 @@
-﻿namespace PheasantTails.TwiHigh.Data.Model.TwiHighUsers
+﻿using FluentValidation;
+
+namespace PheasantTails.TwiHigh.Data.Model.TwiHighUsers
 {
     public class AddTwiHighUserContext
     {
@@ -6,5 +8,28 @@
         public string DisplayName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+    }
+
+    public class AddTwiHighUserContextValidator : AbstractValidator<AddTwiHighUserContext>
+    {
+        public AddTwiHighUserContextValidator()
+        {
+            RuleFor(c => c.DisplayId)
+                .NotEmpty()
+                .OverridePropertyName("ID");
+            RuleFor(c => c.DisplayName)
+                .NotEmpty()
+                .MaximumLength(50)
+                .OverridePropertyName("表示名");
+            RuleFor(c => c.Password)
+                .NotEmpty()
+                .Matches("^([a-zA-Z0-9!-/:-@¥[-`{-~]+)?$")
+                .MinimumLength(8)
+                .OverridePropertyName("パスワード");
+            RuleFor(c => c.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .OverridePropertyName("E-mail");
+        }
     }
 }
