9de6090c0aec407a3cfd570dfc29843d4d9027da
け <challengerk.8000@gmail.com>
2023-03-13 00:38:29 +0900

    update: Updated to feature of sign up.

--- src/PheasantTails.TwiHigh.Client/Pages/Signup.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Pages/Signup.razor.cs
@@ -1,21 +1,70 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using PheasantTails.TwiHigh.Client.TypedHttpClients;
 using PheasantTails.TwiHigh.Data.Model.TwiHighUsers;
 
 namespace PheasantTails.TwiHigh.Client.Pages
 {
     public partial class Signup
     {
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private AppUserHttpClient AppUserHttpClient { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
         private AddTwiHighUserContext Context { get; set; } = new AddTwiHighUserContext();
+
+        private AddTwiHighUserContextValidator Validator { get; } = new AddTwiHighUserContextValidator();
+
         private bool IsWorking { get; set; } = false;
+
         private string ErrorMessage { get; set; } = string.Empty;
+
 
         private async Task OnClickSignupButtonAsync(MouseEventArgs _)
         {
+            if (IsWorking)
+            {
+                return;
+            }
+
             ErrorMessage = string.Empty;
             IsWorking = true;
-            
-            await Task.Delay(500);
+
+            // 暫定的にIDをメール名で取る
+            Context.DisplayId = Context.Email.Split('@').FirstOrDefault() ?? string.Empty;
+
+            // バリデーションの実施
+            var result = Validator.Validate(Context);
+            if (!result.IsValid)
+            {
+                ErrorMessage = result.Errors.FirstOrDefault()?.ErrorMessage ?? "入力項目を見直してください。";
+                IsWorking = false;
+                return;
+            }
+            var response = await AppUserHttpClient.SignInAsync(Context);
+
+            if(string.IsNullOrEmpty(response?.Token))
+            {
+                ErrorMessage = "ペンギンが参加の邪魔をしたようです！";
+                IsWorking = false;
+                return;
+            }
+
+            await ((TwiHighAuthenticationStateProvider)AuthenticationStateProvider).MarkUserAsAuthenticatedAsync(response.Token);
             IsWorking = false;
+            NavigationManager.NavigateTo(DefinePaths.PAGE_PATH_HOME);
         }
     }
 }
