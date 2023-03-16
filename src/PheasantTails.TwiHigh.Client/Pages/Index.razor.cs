e872f265a4e3883223d11b77f9c251b7714ac7ad
け <challengerk.8000@gmail.com>
2023-03-16 21:33:05 +0900

    update: Added navigate to login, signup, logout pages in nav menu.

--- src/PheasantTails.TwiHigh.Client/Pages/Index.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Pages/Index.razor.cs
@@ -1,6 +1,32 @@
-﻿namespace PheasantTails.TwiHigh.Client.Pages
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace PheasantTails.TwiHigh.Client.Pages
 {
     public partial class Index
     {
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private NavigationManager Navigation { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        
+        [CascadingParameter]
+        private Task<AuthenticationState>? AuthenticationState { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (AuthenticationState != null)
+            {
+                var isAuthenticated = (await AuthenticationState).User.Identity?.IsAuthenticated ?? false;
+                if (isAuthenticated)
+                {
+                    Navigation.NavigateTo(DefinePaths.PAGE_PATH_HOME);
+                    return;
+                }
+            }
+
+            await base.OnInitializedAsync();
+            Navigation.NavigateTo(DefinePaths.PAGE_PATH_LOGIN);
+        }
     }
 }
---
+++ src/PheasantTails.TwiHigh.Client/Pages/Logout.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace PheasantTails.TwiHigh.Client.Pages
+{
+    public partial class Logout
+    {
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private NavigationManager Navigation { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ((TwiHighAuthenticationStateProvider)AuthenticationStateProvider).MarkUserAsLoggedOutAsync();
+            Navigation.NavigateTo(DefinePaths.PAGE_PATH_LOGIN);
+            await base.OnInitializedAsync();
+        }
+    }
+}
