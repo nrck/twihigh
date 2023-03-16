04b78e245cac8abe6956e39633a029307e658255
け <challengerk.8000@gmail.com>
2023-03-16 22:11:02 +0900

    update: Added feature of post of tweet by ctrl + enter key.

--- src/PheasantTails.TwiHigh.Client/Components/PostFormComponent.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Components/PostFormComponent.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using PheasantTails.TwiHigh.Client.TypedHttpClients;
 using PheasantTails.TwiHigh.Data.Model;
 
@@ -20,7 +21,12 @@ namespace PheasantTails.TwiHigh.Client.Components
         [Parameter]
         public string UserAvatarUrl { get; set; } = string.Empty;
 
-        public PostTweetContext PostTweetContext { get; set; } = new PostTweetContext();
+        private PostTweetContext PostTweetContext { get; set; } = new PostTweetContext();
+
+        private bool IsPosting { get; set; }
+
+        private string TweetText { get; set; } = string.Empty;
+
 
         protected override async Task OnInitializedAsync()
         {
@@ -31,10 +37,35 @@ namespace PheasantTails.TwiHigh.Client.Components
 
         private async Task OnSubmitAsync()
         {
+            if (string.IsNullOrEmpty(TweetText) || IsPosting)
+            {
+                return;
+            }
+
+            IsPosting = true;
+            StateHasChanged();
+            PostTweetContext.Text = TweetText;
             await TweetHttpClient.PostTweetAsync(PostTweetContext);
             Console.WriteLine("OnSubmit:" + PostTweetContext.Text);
             PostTweetContext.Text = string.Empty;
             PostTweetContext.ReplyTo = null;
+            TweetText = string.Empty;
+            IsPosting = false;
+            StateHasChanged();
+        }
+
+        private async Task OnKeyPressAsync(KeyboardEventArgs e)
+        {
+            if (e.CtrlKey && e.Code == "Enter" && !IsPosting)
+            {
+                await OnSubmitAsync();
+                StateHasChanged();
+            }
+        }
+
+        private void OnInputTextarea(ChangeEventArgs e)
+        {
+            TweetText = e.Value?.ToString() ?? string.Empty;
         }
     }
 }
