9334bdc92e3b76938028cfdd2c5cff13e20b98d1
け <challengerk.8000@gmail.com>
2023-03-20 22:35:13 +0900

    update: Added profile editor page.

---
+++ src/PheasantTails.TwiHigh.Client/Pages/ProfileEditer.razor.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using PheasantTails.TwiHigh.Client.TypedHttpClients;
+using PheasantTails.TwiHigh.Data.Model.TwiHighUsers;
+using System.Net.Mime;
+
+namespace PheasantTails.TwiHigh.Client.Pages
+{
+    public partial class ProfileEditer
+    {
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private AppUserHttpClient AppUserHttpClient { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private NavigationManager Navigation { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [Inject]
+        private AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+        [CascadingParameter]
+        private Task<AuthenticationState> AuthenticationState { get; set; }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
+
+        private ResponseTwiHighUserContext? User { get; set; }
+
+        private string Title { get; set; } = "プロフィール読み込み中";
+
+        private string AvatarUrl { get; set; } = string.Empty;
+
+        private PatchTwiHighUserContext PatchContext { get; set; } = new PatchTwiHighUserContext();
+        private string DisplayId { get; set; } = string.Empty;
+        private string DisplayName { get; set; } = string.Empty;
+        private string Biography { get; set; } = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var id = (await AuthenticationState).User.Claims.FirstOrDefault(c => c.Type == nameof(ResponseTwiHighUserContext.Id))?.Value ?? string.Empty;
+            if (string.IsNullOrEmpty(id))
+            {
+                await ((TwiHighAuthenticationStateProvider)AuthenticationStateProvider).MarkUserAsLoggedOutAsync();
+                Navigation.NavigateTo(DefinePaths.PAGE_PATH_LOGIN);
+            }
+
+            User = await AppUserHttpClient.GetTwiHighUserAsync(id);
+            if (User == null)
+            {
+                await ((TwiHighAuthenticationStateProvider)AuthenticationStateProvider).MarkUserAsLoggedOutAsync();
+                Navigation.NavigateTo(DefinePaths.PAGE_PATH_LOGIN);
+                return;
+            }
+            Title = $"{User.DisplayName}（@{User.DisplayId}）";
+            AvatarUrl = User.AvatarUrl;
+            DisplayId = User.DisplayId;
+            DisplayName = User.DisplayName;
+            Biography = User.Biography;
+            StateHasChanged();
+            await base.OnInitializedAsync();
+        }
+
+        private async Task LoadFiles(InputFileChangeEventArgs e)
+        {
+            var file = e.File;
+            if (
+                file.ContentType != "image/png" &&
+                file.ContentType != "image/jpeg"
+            )
+            {
+                return;
+            }
+
+            if (1 * 1024 * 1024 < file.Size)
+            {
+                return;
+            }
+            byte[] data = new byte[file.Size];
+            var stream = file.OpenReadStream(1 * 1024 * 1024);
+            await stream.ReadAsync(data);
+
+            var base64string = Convert.ToBase64String(data);
+            AvatarUrl = $"data:{file.ContentType};base64,{base64string}";
+            PatchContext.EncodeAvaterImage(file.ContentType, data);
+            StateHasChanged();
+        }
+
+        private async Task OnClickSaveButtonAsync()
+        {
+            AdjustPatchContext();
+            // 送信する
+        }
+
+        private void AdjustPatchContext()
+        {
+            if (DisplayName != User?.DisplayName)
+            {
+                PatchContext.DisplayName = DisplayName;
+            }
+            if (DisplayId != User?.DisplayId)
+            {
+                PatchContext.DisplayId = DisplayId;
+            }
+            if (Biography != User?.Biography)
+            {
+                PatchContext.Biography = Biography;
+            }
+        }
+
+        private void OnClickAvatarResetButton()
+        {
+            PatchContext.Base64EncodedAvatarImage = null;
+            AvatarUrl = User!.AvatarUrl;
+        }
+    }
+}
