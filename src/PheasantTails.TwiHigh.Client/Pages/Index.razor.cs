fa562e894b10eceb2398f6fd1ee235a4cd8a0658
け <challengerk.8000@gmail.com>
2023-03-27 17:29:33 +0900

    add: Apply PageBase to Home, Index.

--- src/PheasantTails.TwiHigh.Client/Pages/Index.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Pages/Index.razor.cs
@@ -1,20 +1,11 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components;
-
-namespace PheasantTails.TwiHigh.Client.Pages
+﻿namespace PheasantTails.TwiHigh.Client.Pages
 {
-    public partial class Index
+    public partial class Index : PageBase
     {
-#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-        [Inject]
-        private NavigationManager Navigation { get; set; }
-#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-
-        [CascadingParameter]
-        private Task<AuthenticationState>? AuthenticationState { get; set; }
-
         protected override async Task OnInitializedAsync()
         {
+            await base.OnInitializedAsync();
+
             if (AuthenticationState != null)
             {
                 var isAuthenticated = (await AuthenticationState).User.Identity?.IsAuthenticated ?? false;
@@ -25,7 +16,6 @@ namespace PheasantTails.TwiHigh.Client.Pages
                 }
             }
 
-            await base.OnInitializedAsync();
             Navigation.NavigateTo(DefinePaths.PAGE_PATH_LOGIN);
         }
     }
