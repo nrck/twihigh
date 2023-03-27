35256e151ee7294c8c43107667b8cf9a70e31258
け <challengerk.8000@gmail.com>
2023-03-27 17:29:33 +0900

    add: Apply PageBase to Home, Index.

--- src/PheasantTails.TwiHigh.Client/Pages/Home.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Pages/Home.razor.cs
@@ -1,7 +1,4 @@
-﻿using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using PheasantTails.TwiHigh.Client.TypedHttpClients;
+﻿using PheasantTails.TwiHigh.Client.TypedHttpClients;
 using PheasantTails.TwiHigh.Data.Model.Timelines;
 using PheasantTails.TwiHigh.Data.Model.TwiHighUsers;
 using PheasantTails.TwiHigh.Data.Store.Entity;
@@ -9,31 +6,8 @@ using System.Net;
 
 namespace PheasantTails.TwiHigh.Client.Pages
 {
-    public partial class Home : IDisposable
+    public partial class Home : PageBase, IDisposable, IAsyncDisposable
     {
-#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-        [Inject]
-        private TimelineHttpClient TimelineHttpClient { get; set; }
-#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-
-#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-        [Inject]
-        private ILocalStorageService LocalStorage { get; set; }
-#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-
-#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-        [Inject]
-        private NavigationManager Navigation { get; set; }
-#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-
-#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-        [Inject]
-        private AuthenticationStateProvider AuthenticationStateProvider { get; set; }
-#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-
-        [CascadingParameter]
-        private Task<AuthenticationState>? AuthenticationState { get; set; }
-
         private Tweet[] Tweets { get; set; } = Array.Empty<Tweet>();
 
         private CancellationTokenSource? WorkerCancellationTokenSource { get; set; } = null;
@@ -42,16 +16,12 @@ namespace PheasantTails.TwiHigh.Client.Pages
 
         protected override async Task OnInitializedAsync()
         {
+            await base.OnInitializedAsync();
             WorkerCancellationTokenSource ??= new CancellationTokenSource();
-            var token = await LocalStorage.GetItemAsStringAsync("TwiHighJwt");
             AvatarUrl = await GetMyAvatarUrlAsync();
             StateHasChanged();
-            TimelineHttpClient.SetToken(token);
 
-            await Task.WhenAll(
-                base.OnInitializedAsync(),
-                GetMyTimerlineEvery5secAsync(WorkerCancellationTokenSource.Token)
-            );
+            await GetMyTimerlineEvery5secAsync(WorkerCancellationTokenSource.Token);
         }
 
         private async Task GetMyTimerlineEvery5secAsync(CancellationToken cancellationToken = default)
@@ -97,10 +67,13 @@ namespace PheasantTails.TwiHigh.Client.Pages
             return (await AuthenticationState).User.Claims.FirstOrDefault(c => c.Type == nameof(ResponseTwiHighUserContext.AvatarUrl))?.Value ?? string.Empty;
         }
 
-        public void Dispose()
+        protected override void Dispose(bool disposing)
         {
-            WorkerCancellationTokenSource?.Cancel();
-            GC.SuppressFinalize(this);
+            if (disposing)
+            {
+                WorkerCancellationTokenSource?.Cancel();
+                base.Dispose(disposing);
+            }
         }
     }
 }
