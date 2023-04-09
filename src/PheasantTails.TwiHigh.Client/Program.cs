436563f4735ec5e3d8f26eb637350241a7c82b58
け <challengerk.8000@gmail.com>
2023-04-09 22:51:02 +0900

    update: Client version 0.3.3 -> 0.3.4

--- src/PheasantTails.TwiHigh.Client/Program.cs
+++ src/PheasantTails.TwiHigh.Client/Program.cs
@@ -9,7 +9,7 @@ namespace PheasantTails.TwiHigh.Client
 {
     public class Program
     {
-        public static string TwiHighVersion => "0.3.3";
+        public static string TwiHighVersion => "0.3.4";
 
         public static async Task Main(string[] args)
         {
