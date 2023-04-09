0331cbdcc5126a54b91be48f68c99d7ab7c97079
け <challengerk.8000@gmail.com>
2023-04-09 18:54:36 +0900

    update: Supported Timeline api v2.

--- src/PheasantTails.TwiHigh.Client/TypedHttpClients/TimelineHttpClient.cs
+++ src/PheasantTails.TwiHigh.Client/TypedHttpClients/TimelineHttpClient.cs
@@ -1,6 +1,8 @@
 ﻿using PheasantTails.TwiHigh.Data.Model.Timelines;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Web;
 
 namespace PheasantTails.TwiHigh.Client.TypedHttpClients
 {
@@ -8,6 +10,7 @@ namespace PheasantTails.TwiHigh.Client.TypedHttpClients
     {
         private const string API_URL_BASE = "https://twihigh-dev-apim.azure-api.net/timelines";
         private const string API_URL_TIMELINE = $"{API_URL_BASE}/GetMyTimeline";
+        private const string API_URL_TIMELINE_V2 = $"{API_URL_BASE}/timeline?since={{0}}&until={{1}}";
         private readonly HttpClient _httpClient;
 
         public TimelineHttpClient(HttpClient httpClient)
@@ -28,6 +31,25 @@ namespace PheasantTails.TwiHigh.Client.TypedHttpClients
         public async Task<ResponseTimelineContext?> GetMyTimelineAsync()
         {
             return await _httpClient.GetFromJsonAsync<ResponseTimelineContext>(API_URL_TIMELINE);
+        }
+
+        public async Task<ResponseTimelineContext?> GetMyTimelineAsync(DateTimeOffset since, DateTimeOffset until)
+        {
+            if (until < since)
+            {
+                throw new ArgumentException("開始時刻と終了時刻が逆転しています。", nameof(since));
+            }
+
+            var url = string.Format(API_URL_TIMELINE_V2, HttpUtility.UrlEncode(since.ToString("yyyy-MM-ddTHH:mm:ss.fffffffzzz")), HttpUtility.UrlEncode(until.ToString("yyyy-MM-ddTHH:mm:ss.fffffffzzz")));
+            var response = await _httpClient.GetAsync(url);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return await response.Content.ReadFromJsonAsync<ResponseTimelineContext>();
+            }
+
+            response.EnsureSuccessStatusCode();
+            var context = new ResponseTimelineContext { Latest = DateTimeOffset.UtcNow, Oldest = DateTimeOffset.UtcNow };
+            return context;
         }
     }
 }
