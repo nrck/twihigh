573938950ff4c33924b9889e9e84a9570c8171ce
け <challengerk.8000@gmail.com>
2023-04-09 22:49:25 +0900

    add: GapTweetsLoadButtonComponent.razor

---
+++ src/PheasantTails.TwiHigh.Client/Components/GapTweetsLoadButtonComponent.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace PheasantTails.TwiHigh.Client.Components
+{
+    public partial class GapTweetsLoadButtonComponent
+    {
+        [Parameter]
+        public int Index { get; set; }
+
+        [Parameter]
+        public DateTimeOffset Since { get; set; }
+
+        [Parameter]
+        public DateTimeOffset Until { get; set; }
+
+        [Parameter]
+        public OnClickGetGapTweetsAsync? OnClick { get; set; }
+
+        public delegate Task OnClickGetGapTweetsAsync(int index, DateTimeOffset since, DateTimeOffset until);
+
+        private void OnClickGapButton(MouseEventArgs _)
+        {
+            OnClick?.Invoke(Index, Since, Until);
+        }
+    }
+}
