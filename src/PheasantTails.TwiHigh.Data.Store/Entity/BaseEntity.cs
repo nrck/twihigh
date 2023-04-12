caa44ba4380098ded8774a4d44633373fdd3213d
け <challengerk.8000@gmail.com>
2023-04-12 23:15:52 +0900

    update: Add properties.
    - UpdateAt in BaseEntitiy
    - IsDeleted in Tweet

--- src/PheasantTails.TwiHigh.Data.Store/Entity/BaseEntity.cs
+++ src/PheasantTails.TwiHigh.Data.Store/Entity/BaseEntity.cs
@@ -2,7 +2,8 @@
 {
     public abstract class BaseEntity
     {
-        public virtual Guid Id { get; set; } = Guid.Empty;
+        public virtual Guid Id { get; set; } = Guid.NewGuid();
+        public virtual DateTimeOffset UpdateAt { get; set; } = DateTimeOffset.UtcNow;
         public virtual DateTimeOffset CreateAt { get; set; } = DateTimeOffset.UtcNow;
     }
 }
