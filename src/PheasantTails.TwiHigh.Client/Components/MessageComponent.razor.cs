19010349ab74226a8830b89b6be6580ba6769e5c
け <challengerk.8000@gmail.com>
2023-04-30 23:27:09 +0900

    update: Added MessageComponent.

---
+++ src/PheasantTails.TwiHigh.Client/Components/MessageComponent.razor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace PheasantTails.TwiHigh.Client.Components
+{
+    public partial class MessageComponent : IAsyncDisposable
+    {
+        public enum MessageLevel
+        {
+            Info,
+            Success,
+            Warn,
+            Error
+        }
+
+        [Parameter]
+        public Guid MessageId { get; set; }
+
+        [Parameter]
+        public string Message { get; set; } = string.Empty;
+
+        [Parameter]
+        public MessageLevel Level { get; set; } = MessageLevel.Info;
+
+        [Parameter]
+        public EventCallback<Guid> OnClickClose { get; set; }
+
+        private string MessageAreaCssClass => Level switch
+        {
+            MessageLevel.Success => "success",
+            MessageLevel.Error => "error",
+            MessageLevel.Warn => "warn",
+            _ => "info",
+        };
+
+        private bool IsDispose { get; set; }
+
+        private void OnClickCloseButton(MouseEventArgs e)
+        {
+            OnClickClose.InvokeAsync(MessageId);
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            if (IsDispose)
+            {
+                return ValueTask.CompletedTask;
+            }
+            IsDispose = true;
+            StateHasChanged();
+            var task = Task.Run(async () => {
+                await Task.Delay(1000);
+                GC.SuppressFinalize(this);
+            });
+            return new ValueTask(task);
+        }
+    }
+}
