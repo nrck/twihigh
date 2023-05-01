68081f962be63aebcd6581dff4e10ef9bbc2d371
け <challengerk.8000@gmail.com>
2023-05-01 21:21:12 +0900

    update: Updated TweetComponent, and added TimelineComponent

--- src/PheasantTails.TwiHigh.Client/Components/TweetComponent.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Components/TweetComponent.razor.cs
@@ -1,60 +1,59 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using PheasantTails.TwiHigh.Client.Pages;
+using PheasantTails.TwiHigh.Client.ViewModels;
 using PheasantTails.TwiHigh.Data.Model;
-using PheasantTails.TwiHigh.Data.Store.Entity;
 
 namespace PheasantTails.TwiHigh.Client.Components
 {
-    public partial class TweetComponent
+    public partial class TweetComponent : UIComponentBase
     {
+        /// <summary>
+        /// 表示するツイート
+        /// </summary>
         [Parameter]
-        public Tweet? Tweet { get; set; }
+        public TweetViewModel? Tweet { get; set; }
 
+        /// <summary>
+        /// リプライが投稿されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<PostTweetContext> OnPostReply { get; set; }
+
+        /// <summary>
+        /// ツイート削除ボタンが押下されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickDelete { get; set; }
+
+        /// <summary>
+        /// ツイート詳細が押下されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickDetail { get; set; }
+
+        /// <summary>
+        /// リツイートが押下されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickRetweet { get; set; }
+
+        /// <summary>
+        /// お気に入りが押下されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickFavorite { get; set; }
+
+        /// <summary>
+        /// プロフィール欄への遷移が発生したときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickProfile { get; set; }
+
+        /// <summary>
+        /// 表示しているツイートが自身のものなら<c>true</c>
+        /// </summary>
         [Parameter]
         public bool IsMyTweet { get; set; }
-
-#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-        [Parameter]
-        public Action<Guid> OnDeleteAction { get; set; }
-#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-
-#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-        [Inject]
-        private NavigationManager Navigation { get; set; }
-#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
-
-        private string CreateAt
-        {
-            get
-            {
-                if (Tweet == null)
-                {
-                    return string.Empty;
-                }
-
-                // 1年前ならyyyy/mm/dd
-                if (Tweet.CreateAt <= DateTimeOffset.UtcNow.AddYears(-1))
-                {
-                    return Tweet.CreateAt.ToLocalTime().ToString("yyyy/MM/dd");
-                }
-
-                // 24時間より前ならm/d
-                if (Tweet.CreateAt <= DateTimeOffset.UtcNow.AddDays(-1))
-                {
-                    return Tweet.CreateAt.ToLocalTime().ToString("M/d");
-                }
-
-                // 1時間より前なら H:mm
-                if (Tweet.CreateAt <= DateTimeOffset.UtcNow.AddHours(-1))
-                {
-                    return Tweet.CreateAt.ToLocalTime().ToString("H:mm");
-                }
-
-                // H:mm:ss
-                return Tweet.CreateAt.ToLocalTime().ToString("H:mm:ss");
-            }
-        }
 
         private bool IsOpendReplyPostForm { get; set; }
 
@@ -76,37 +75,16 @@ namespace PheasantTails.TwiHigh.Client.Components
             }
         }
 
-        private void OnClickAvatar(MouseEventArgs _) => NavigateToProfilePage();
+        private Task OnClickAvatar(MouseEventArgs _) => OnClickProfile.InvokeAsync(Tweet);
 
-        private void OnClickUserDisplayName(MouseEventArgs _) => NavigateToProfilePage();
+        private Task OnClickUserDisplayName(MouseEventArgs _) => OnClickProfile.InvokeAsync(Tweet);
 
-        private void OnClickUserDisplayId(MouseEventArgs _) => NavigateToProfilePage();
+        private Task OnClickUserDisplayId(MouseEventArgs _) => OnClickProfile.InvokeAsync(Tweet);
 
-        private void NavigateToProfilePage()
-        {
-            if (Tweet == null)
-            {
-                return;
-            }
+        private Task OnClickDeleteButton(MouseEventArgs _) => OnClickDelete.InvokeAsync(Tweet);
 
-            var url = string.Format(DefinePaths.PAGE_PATH_PROFILE, Tweet.UserDisplayId);
-            Navigation.NavigateTo(url);
-        }
+        private void OnClickReplyButton(MouseEventArgs _) => IsOpendReplyPostForm = true;
 
-        private void OnClickDeleteButton(MouseEventArgs _)
-        {
-            OnDeleteAction?.Invoke(Tweet!.Id);
-        }
-
-
-        private void OnClickReplyButton(MouseEventArgs _)
-        {
-            IsOpendReplyPostForm = true;
-        }
-
-        private void OnClickReplyPostCloseButton(MouseEventArgs _)
-        {
-            IsOpendReplyPostForm = false;
-        }
+        private void OnClickReplyPostCloseButton(MouseEventArgs _) => IsOpendReplyPostForm = false;
     }
 }
