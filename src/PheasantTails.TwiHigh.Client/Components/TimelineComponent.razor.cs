1d5b8ff3a179c6a3846bc7479ee10d3942486e76
け <challengerk.8000@gmail.com>
2023-05-01 21:21:12 +0900

    update: Updated TweetComponent, and added TimelineComponent

---
+++ src/PheasantTails.TwiHigh.Client/Components/TimelineComponent.razor.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Components;
+using PheasantTails.TwiHigh.Client.ViewModels;
+using PheasantTails.TwiHigh.Data.Model;
+
+namespace PheasantTails.TwiHigh.Client.Components
+{
+    public partial class TimelineComponent : UIComponentBase
+    {
+        /// <summary>
+        /// タイムラインに表示するツイートList
+        /// </summary>
+        [Parameter]
+        public List<TweetViewModel> Tweets { get; set; } = new List<TweetViewModel>();
+
+        /// <summary>
+        /// リプライが投稿されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<PostTweetContext> OnPostReply { get; set; }
+
+        /// <summary>
+        /// ツイート削除ボタンが押下されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickDelete { get; set; }
+
+        /// <summary>
+        /// ツイート詳細が押下されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickDetail { get; set; }
+
+        /// <summary>
+        /// リツイートが押下されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickRetweet { get; set; }
+
+        /// <summary>
+        /// お気に入りが押下されたときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickFavorite { get; set; }
+
+        /// <summary>
+        /// プロフィール欄への遷移が発生したときに発火するイベントコールバック
+        /// </summary>
+        [Parameter]
+        public EventCallback<TweetViewModel> OnClickProfile { get; set; }
+
+        private Task OnClickReplyAsync(PostTweetContext tweet) => OnPostReply.InvokeAsync(tweet);
+
+        private Task OnClickDeleteAsync(TweetViewModel tweet) => OnClickDelete.InvokeAsync(tweet);
+
+        private Task OnClickDetailAsync(TweetViewModel tweet) => OnClickDetail.InvokeAsync(tweet);
+
+        private Task OnClickRetweetAsync(TweetViewModel tweet) => OnClickRetweet.InvokeAsync(tweet);
+
+        private Task OnClickFavoriteAsync(TweetViewModel tweet) => OnClickFavorite.InvokeAsync(tweet);
+
+        private Task OnClickProfileAsync(TweetViewModel tweet) => OnClickProfile.InvokeAsync(tweet);
+    }
+}
