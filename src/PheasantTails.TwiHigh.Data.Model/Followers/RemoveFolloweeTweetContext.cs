a79356974a5114b6ce929a5eda1e14cc51c83cd7
け <challengerk.8000@gmail.com>
2023-07-03 19:49:14 +0900

    update: Add feature of remove followee.

---
+++ src/PheasantTails.TwiHigh.Data.Model/Followers/RemoveFolloweeTweetContext.cs
@@ -0,0 +1,8 @@
+﻿namespace PheasantTails.TwiHigh.Data.Model.Followers
+{
+    public class RemoveFolloweeTweetContext
+    {
+        public Guid FolloweeId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
