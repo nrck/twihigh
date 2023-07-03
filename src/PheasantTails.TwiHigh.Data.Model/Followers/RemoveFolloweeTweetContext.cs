beb3551c983f87cc2f6138c5c73af33381638a24
け <challengerk.8000@gmail.com>
2023-07-03 23:13:18 +0900

    update: Add feature of remove followee.

--- src/PheasantTails.TwiHigh.Data.Model/Followers/RemoveFolloweeTweetContext.cs
+++ src/PheasantTails.TwiHigh.Data.Model/Followers/RemoveFolloweeTweetContext.cs
@@ -2,7 +2,14 @@
 {
     public class RemoveFolloweeTweetContext
     {
+        /// <summary>
+        /// リムーブ対象のフォローユーザID
+        /// </summary>
         public Guid FolloweeId { get; set; }
+
+        /// <summary>
+        /// 操作した人のユーザID
+        /// </summary>
         public Guid UserId { get; set; }
     }
 }
