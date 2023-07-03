9101b71356eb26c2dfdbcb4fe9fe519d5dd10d26
け <challengerk.8000@gmail.com>
2023-07-03 19:49:14 +0900

    update: Add feature of remove followee.

--- src/PheasantTails.TwiHigh.Functions.Follows/FollowFunction.cs
+++ src/PheasantTails.TwiHigh.Functions.Follows/FollowFunction.cs
@@ -8,6 +8,7 @@ using PheasantTails.TwiHigh.Data.Model.Followers;
 using PheasantTails.TwiHigh.Data.Store.Entity;
 using PheasantTails.TwiHigh.Functions.Core;
 using PheasantTails.TwiHigh.Functions.Extensions;
+using System;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -25,6 +26,7 @@ namespace PheasantTails.TwiHigh.Functions.Follows
             _logger = log;
             _client = client;
         }
+
         [FunctionName("AddFollow")]
         public async Task<IActionResult> AddFollow(
             [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "{followeeUserId}")] HttpRequest req,
@@ -59,7 +61,60 @@ namespace PheasantTails.TwiHigh.Functions.Follows
                     AZURE_STORAGE_ADD_TIMELINES_FOLLOW_TRIGGER_QUEUE_NAME,
                     new AddNewFolloweeTweetContext { FolloweeId = followee.Resource.Id, UserId = follower.Resource.Id });
 
-            return new OkResult();
+            return new NoContentResult();
+        }
+
+        [FunctionName("RemoveFollow")]
+        public async Task<IActionResult> RemoveFollow(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "{followeeUserId}")] HttpRequest req,
+            string followeeUserId)
+        {
+            if (!req.TryGetUserId(out var followerUserId))
+            {
+                return new UnauthorizedResult();
+            }
+
+            var users = _client.GetContainer(TWIHIGH_COSMOSDB_NAME, TWIHIGH_USER_CONTAINER_NAME);
+
+            var followee = await users.ReadItemAsync<TwiHighUser>(followeeUserId, new PartitionKey(followeeUserId));
+            if (followee.StatusCode != HttpStatusCode.OK)
+            {
+                return new NotFoundResult();
+            }
+
+            var follower = await users.ReadItemAsync<TwiHighUser>(followerUserId, new PartitionKey(followerUserId));
+            if (follower.StatusCode != HttpStatusCode.OK)
+            {
+                return new BadRequestResult();
+            }
+
+            var followeeIndex = Array.FindIndex(followee.Resource.Followers, (id) => id == follower.Resource.Id);
+            if (0 <= followeeIndex)
+            {
+                var patch = new[]
+                {
+                    PatchOperation.Remove($"/followers/{followeeIndex}"),
+                    PatchOperation.Set("/updateAt", DateTimeOffset.Now)
+                };
+                await users.PatchItemAsync<Tweet>(followeeUserId, new PartitionKey(followeeUserId), patch);
+            }
+
+            var followerIndex = Array.FindIndex(follower.Resource.Follows, (id) => id == followee.Resource.Id);
+            if (0 <= followerIndex)
+            {
+                var patch = new[]
+                {
+                    PatchOperation.Remove($"/follows/{followerIndex}"),
+                    PatchOperation.Set("/updateAt", DateTimeOffset.Now)
+                };
+                await users.PatchItemAsync<Tweet>(followerUserId, new PartitionKey(followerUserId), patch);
+            }
+
+            await QueueStorages.InsertMessageAsync(
+                    AZURE_STORAGE_DELETE_TIMELINES_FOLLOW_TRIGGER_QUEUE_NAME,
+                    new RemoveFolloweeTweetContext { FolloweeId = followee.Resource.Id, UserId = follower.Resource.Id });
+
+            return new NoContentResult();
         }
     }
 }
