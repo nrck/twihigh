51c645e3e284ca987ccafb7e4b942b4c4ec725b4
け <challengerk.8000@gmail.com>
2023-07-03 16:32:15 +0900

    update: deploy-follows-function.yml

--- src/PheasantTails.TwiHigh.Data.Store/Entity/BaseEntity.cs
+++ src/PheasantTails.TwiHigh.Data.Store/Entity/BaseEntity.cs
@@ -1,9 +1,23 @@
 ﻿namespace PheasantTails.TwiHigh.Data.Store.Entity
 {
+    /// <summary>
+    /// CosmosDB 基本クラス
+    /// </summary>
     public abstract class BaseEntity
     {
+        /// <summary>
+        /// アイテムID
+        /// </summary>
         public virtual Guid Id { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// データ更新日時
+        /// </summary>
         public virtual DateTimeOffset UpdateAt { get; set; } = DateTimeOffset.UtcNow;
+
+        /// <summary>
+        /// データ作成日時
+        /// </summary>
         public virtual DateTimeOffset CreateAt { get; set; } = DateTimeOffset.UtcNow;
     }
 }
