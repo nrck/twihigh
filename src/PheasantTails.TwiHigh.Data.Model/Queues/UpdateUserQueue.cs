c5da47510e8f5c78deb775d32bb66b09dcefc578
け <challengerk.8000@gmail.com>
2023-07-10 17:47:32 +0900

    update: Added feature of update tweet triggered by user info updated.

---
+++ src/PheasantTails.TwiHigh.Data.Model/Queues/UpdateUserQueue.cs
@@ -0,0 +1,16 @@
+﻿using PheasantTails.TwiHigh.Data.Store.Entity;
+
+namespace PheasantTails.TwiHigh.Data.Model.Queues
+{
+    public class UpdateUserQueue
+    {
+        public TwiHighUser TwiHighUser { get; set; }
+
+        public UpdateUserQueue(TwiHighUser twiHighUser) 
+        {
+            TwiHighUser = twiHighUser;
+        }
+
+        public static implicit operator UpdateUserQueue(TwiHighUser twiHighUser) => new(twiHighUser);
+    }
+}
