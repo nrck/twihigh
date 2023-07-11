ee733a7c19ddadd8c307b5ae29957e60111e0af1
け <challengerk.8000@gmail.com>
2023-07-12 00:58:09 +0900

    fix: AutoLink

--- src/PheasantTails.TwiHigh.Client/Shared/AutoLink.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Shared/AutoLink.razor.cs
@@ -14,22 +14,48 @@ namespace PheasantTails.TwiHigh.Client.Shared
         [Parameter]
         public bool ReplaceUrl { get; set; } = true;
 
-        private string Content { get; set; } = string.Empty;
+        private string ContentString { get; set; } = string.Empty;
+
+        private string[] ContentArray { get; set; } = Array.Empty<string>();
 
         protected override void OnParametersSet()
         {
-            Content = Text;
+            ContentString = Text;
             if (ReplaceDisplayId)
             {
-                Content = Regex.Replace(Content, "@([a-zA-Z0-9._-]+)", "<a href=\"profile/$1\">@$1</a>");
+                //Content = Regex.Replace(Content, "@([a-zA-Z0-9._-]+)", "<a href=\"/profile/$1\">@$1</a>");
+                ContentString = Regex.Replace(ContentString, "@([a-zA-Z0-9._-]+)", $"{Environment.NewLine}$1{Environment.NewLine}");
             }
             if (ReplaceUrl)
             {
-                Content = Regex.Replace(Content, "(https?://[\\w/:%#\\$&\\?\\(\\)~\\.=\\+\\-]+)", " <a href=\"$1\" target=\"_blank\">$1</a> ");
+                ContentString = Regex.Replace(ContentString, "(https?://[\\w/:%#\\$&\\?\\(\\)~\\.=\\+\\-]+)", " <a href=\"$1\" target=\"_blank\" onclick=\"event.stopPropagation()\">$1</a> ");
             }
+
+            ContentArray = ContentString.Split(Environment.NewLine);
 
             StateHasChanged();
             base.OnParametersSet();
         }
+
+        private RenderFragment GetRenderFragment() => builder =>
+        {
+            var sequence = 0;
+            for (int index = 0; index < ContentArray.Length; index++)
+            {
+                if (index % 2 == 0 )
+                {
+                    builder.AddMarkupContent(sequence, ContentArray[index]);
+                    sequence++;
+                }
+                else
+                {
+                    builder.OpenComponent<UserIdLink>(sequence);
+                    sequence++;
+                    builder.AddAttribute(sequence, "UserDisplayId", ContentArray[index]);
+                    sequence++;
+                    builder.CloseComponent();
+                }
+            }
+        };
     }
 }
