f1a4780aa787010bfe0402457410f6a061803024
け <challengerk.8000@gmail.com>
2023-07-16 14:01:20 +0900

    update: Added Licence page.

---
+++ src/PheasantTails.TwiHigh.Client/Components/LicenceComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using PheasantTails.TwiHigh.Client.ViewModels;
+
+namespace PheasantTails.TwiHigh.Client.Components
+{
+    public partial class LicenceComponent : UIComponentBase
+    {
+        [Parameter]
+        public Licence Licence { get; set; }
+    }
+}
