879b519af790e01bbc53cae12475650e63e1a05f
け <challengerk.8000@gmail.com>
2023-09-05 19:48:21 +0900

    add: PatchTweetInTimelines

---
+++ src/PheasantTails.TwiHigh.Functions.Timelines/QueueTriggers/PatchTweetInTimelines.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using PheasantTails.TwiHigh.Data.Model.Queues;
+using PheasantTails.TwiHigh.Functions.Core.Extensions;
+using PheasantTails.TwiHigh.Functions.Timelines.Helpers;
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static PheasantTails.TwiHigh.Functions.Core.StaticStrings;
+
+namespace PheasantTails.TwiHigh.Functions.Timelines.QueueTriggers
+{
+    public class PatchTweetInTimelines
+    {
+        private const string FUNCTION_NAME = "PatchTweetInTimelines";
+        private readonly CosmosClient _client;
+
+        public PatchTweetInTimelines(CosmosClient client)
+        {
+            _client = client;
+        }
+
+        [FunctionName(FUNCTION_NAME)]
+        public async Task PatchTweetInTimelinesAsync(
+            [QueueTrigger(AZURE_STORAGE_PATCH_TWEET_IN_TIMELINES_QUEUE_NAME, Connection = QUEUE_STORAGE_CONNECTION_STRINGS_ENV_NAME)] string myQueueItem,
+            ILogger logger)
+        {
+            try
+            {
+                logger.TwiHighLogStart(FUNCTION_NAME);
+                if (myQueueItem == null)
+                {
+                    // null check
+                    throw new ArgumentNullException(nameof(myQueueItem), "Queue is Null");
+                }
+
+                var que = JsonSerializer.Deserialize<PatchTweetQueue>(myQueueItem);
+                var batchResult = await _client.PatchTimelineAsync(que.TweetId, que.Operations);
+
+                logger.TwiHighLogInformation(FUNCTION_NAME, "PatchTimelineAsync batch finish. RU:{0}, Task Count:{1}, Success:{2}",
+                    batchResult.Sum(r => r.Headers.RequestCharge),
+                    batchResult.LongLength,
+                    batchResult.LongCount(r => r.IsSuccessStatusCode));
+            }
+            catch (Exception ex)
+            {
+                logger.TwiHighLogError(FUNCTION_NAME, ex);
+                throw;
+            }
+            finally
+            {
+                logger.TwiHighLogEnd(FUNCTION_NAME);
+            }
+        }
+    }
+}
