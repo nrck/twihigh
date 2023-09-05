d4d73d1d25a788bbd3e2b4d561d2b24c886d21ba
け <challengerk.8000@gmail.com>
2023-09-05 19:39:22 +0900

    add: DeleteTimelinesFollowTrigger

--- src/PheasantTails.TwiHigh.Functions.Timelines/QueueTriggers/DeleteTimelinesFollowTrigger.cs
+++ src/PheasantTails.TwiHigh.Functions.Timelines/QueueTriggers/DeleteTimelinesFollowTrigger.cs
@@ -1,63 +1,39 @@
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Cosmos;
+Ôªøusing Microsoft.Azure.Cosmos;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using PheasantTails.TwiHigh.Data.Model.Followers;
-using PheasantTails.TwiHigh.Data.Model.Queues;
-using PheasantTails.TwiHigh.Data.Model.Timelines;
-using PheasantTails.TwiHigh.Data.Store.Entity;
-using PheasantTails.TwiHigh.Functions.Extensions;
-using PheasantTails.TwiHigh.Functions.Timelines.Helpers;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using static PheasantTails.TwiHigh.Functions.Core.StaticStrings;
 
-namespace PheasantTails.TwiHigh.Functions.Timelines
+namespace PheasantTails.TwiHigh.Functions.Timelines.QueueTriggers
 {
-    public class TimelineFunction
+    public  class DeleteTimelinesFollowTrigger
     {
-        private readonly ILogger<TimelineFunction> _logger;
+        private const string FUNCTION_NAME = "DeleteTimelinesFollowTrigger";
         private readonly CosmosClient _client;
-        private readonly TokenValidationParameters _tokenValidationParameters;
 
-        public TimelineFunction(CosmosClient client, ILogger<TimelineFunction> log, TokenValidationParameters tokenValidationParameters)
+        public DeleteTimelinesFollowTrigger(CosmosClient client)
         {
-            _logger = log;
             _client = client;
-            _tokenValidationParameters = tokenValidationParameters;
         }
 
-
-
-
-
-
-        
-
-
-        
-
-
-        [FunctionName("DeleteTimelinesFollowTrigger")]
-        public async Task DeleteTimelinesFollowTriggerAsync([QueueTrigger(AZURE_STORAGE_DELETE_TIMELINES_FOLLOW_TRIGGER_QUEUE_NAME, Connection = QUEUE_STORAGE_CONNECTION_STRINGS_ENV_NAME)] string myQueueItem)
+        [FunctionName(FUNCTION_NAME)]
+        public async Task DeleteTimelinesFollowTriggerAsync(
+            [QueueTrigger(AZURE_STORAGE_DELETE_TIMELINES_FOLLOW_TRIGGER_QUEUE_NAME, Connection = QUEUE_STORAGE_CONNECTION_STRINGS_ENV_NAME)] string myQueueItem,
+            ILogger logger)
         {
             if (myQueueItem == null)
             {
                 return;
             }
 
-            // ÉLÉÖÅ[ÇÃéÊìæ
+            // „Ç≠„É•„Éº„ÅÆÂèñÂæó
             var que = JsonSerializer.Deserialize<RemoveFolloweeTweetContext>(myQueueItem);
             var now = DateTimeOffset.UtcNow;
 
-            _logger.LogInformation("{0} remove to {1} at {2}.", que.UserId, que.FolloweeId, now.ToString("yyyy/MM/dd HH:mm:ss"));
+            logger.LogInformation("{0} remove to {1} at {2}.", que.UserId, que.FolloweeId, now.ToString("yyyy/MM/dd HH:mm:ss"));
             var patch = new PatchOperation[]
             {
                 PatchOperation.Set("/text", "This tweet has been deleted."),
@@ -65,7 +41,7 @@ namespace PheasantTails.TwiHigh.Functions.Timelines
                 PatchOperation.Set("/updateAt", now)
             };
 
-            // ÉtÉHÉçÅ[ÇäOÇµÇΩêlÇÃÉcÉCÅ[ÉgÇéÊìæÇ∑ÇÈ
+            // „Éï„Ç©„É≠„Éº„ÇíÂ§ñ„Åó„Åü‰∫∫„ÅÆ„ÉÑ„Ç§„Éº„Éà„ÇíÂèñÂæó„Åô„Çã
             var query = new QueryDefinition(
                 "SELECT c.id, c.ownerUserId FROM c " +
                 "WHERE c.userId = @FolloweeId " +
@@ -86,11 +62,11 @@ namespace PheasantTails.TwiHigh.Functions.Timelines
                         patchOperations: patch,
                         requestOptions: new TransactionalBatchPatchItemRequestOptions { IfMatchEtag = result.ETag }
                     );
-                    _logger.LogInformation("id: {0}, ownerUserId:{1}", pair.Id, pair.OwnerUserId);
+                    logger.LogInformation("id: {0}, ownerUserId:{1}", pair.Id, pair.OwnerUserId);
                 }
             }
             var response = await batch.ExecuteAsync();
-            _logger.LogInformation("Batch status code:{0}, RU:{1}", response.StatusCode, response.RequestCharge);
+            logger.LogInformation("Batch status code:{0}, RU:{1}", response.StatusCode, response.RequestCharge);
         }
     }
 }
