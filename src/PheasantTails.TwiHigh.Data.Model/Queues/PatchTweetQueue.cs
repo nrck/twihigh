5b409c80970061779f0f30fe015612736a5e36e9
け <challengerk.8000@gmail.com>
2023-09-06 10:36:11 +0900

    update: Repository

--- src/PheasantTails.TwiHigh.Data.Model/Queues/PatchTweetQueue.cs
+++ src/PheasantTails.TwiHigh.Data.Model/Queues/PatchTweetQueue.cs
@@ -4,7 +4,14 @@ namespace PheasantTails.TwiHigh.Data.Model.Queues
 {
     public class PatchTweetQueue
     {
+        /// <summary>
+        /// Target tweet id
+        /// </summary>
         public Guid TweetId { get; set; }
+
+        /// <summary>
+        /// Patch operation
+        /// </summary>
         public PatchOperation[] Operations { get; set; } = Array.Empty<PatchOperation>();
     }
 }
