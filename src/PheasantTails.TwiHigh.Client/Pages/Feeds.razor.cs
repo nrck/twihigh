dbec7e96909a4947944519bc2249385a9aa07c77
け <challengerk.8000@gmail.com>
2023-09-15 16:27:38 +0900

    update: added detail and profile link

--- src/PheasantTails.TwiHigh.Client/Pages/Feeds.razor.cs
+++ src/PheasantTails.TwiHigh.Client/Pages/Feeds.razor.cs
@@ -1,4 +1,5 @@
 ﻿using PheasantTails.TwiHigh.Client.TypedHttpClients;
+using PheasantTails.TwiHigh.Client.ViewModels;
 using PheasantTails.TwiHigh.Data.Model.Feeds;
 
 namespace PheasantTails.TwiHigh.Client.Pages
@@ -21,5 +22,11 @@ namespace PheasantTails.TwiHigh.Client.Pages
             }
             StateHasChanged();
         }
+
+        private void OnClickDetail(TweetViewModel tweetViewModel) => 
+            Navigation.NavigateTo(string.Format(DefinePaths.PAGE_PATH_STATUS, tweetViewModel.UserDisplayId, tweetViewModel.Id));
+
+        private void OnClickProfile(TweetViewModel tweetViewModel) =>
+            Navigation.NavigateTo(string.Format(DefinePaths.PAGE_PATH_PROFILE, tweetViewModel.UserDisplayId));
     }
 }
