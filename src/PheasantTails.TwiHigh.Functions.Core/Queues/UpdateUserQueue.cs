922bea9099358bf16ff90e928a2d5a9f84f587bb
け <challengerk.8000@gmail.com>
2023-10-02 14:57:04 +0900

    move: Queue model moved model project to core project.

---
+++ src/PheasantTails.TwiHigh.Functions.Core/Queues/UpdateUserQueue.cs
@@ -0,0 +1,13 @@
+﻿using PheasantTails.TwiHigh.Interface;
+
+namespace PheasantTails.TwiHigh.Data.Model.Queues;
+
+public class UpdateUserQueue
+{
+    public ITwiHighUser TwiHighUser { get; set; }
+
+    public UpdateUserQueue(ITwiHighUser twiHighUser) 
+    {
+        TwiHighUser = twiHighUser;
+    }
+}
