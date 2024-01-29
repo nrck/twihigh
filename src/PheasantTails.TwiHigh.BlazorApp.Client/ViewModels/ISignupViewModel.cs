858fe53631c4a7b83bd4e566f1b922184c2ddc51
け <challengerk.8000@gmail.com>
2024-01-29 18:05:02 +0900

    update: Signup migrated.

---
+++ src/PheasantTails.TwiHigh.BlazorApp.Client/ViewModels/ISignupViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Reactive.Bindings;
+
+namespace PheasantTails.TwiHigh.BlazorApp.Client.ViewModels;
+
+public interface ISignupViewModel : IViewModelBase
+{
+    ReactivePropertySlim<bool> CanSignupCommand { get; }
+    ReactivePropertySlim<string> DisplayId { get; }
+    ReactivePropertySlim<string> Email { get; }
+    ReactivePropertySlim<string> Password { get; }
+    AsyncReactiveCommand SignupCommand { get; }
+}
