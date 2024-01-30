3a35d09e17fd4202cb3a6b57f3e1f39e65adec85
け <challengerk.8000@gmail.com>
2024-01-30 22:48:07 +0900

    WIP

---
+++ src/PheasantTails.TwiHigh.BlazorApp.Client/Views/Components/THFollowButton.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using PheasantTails.TwiHigh.BlazorApp.Client.Views.Bases;
+
+namespace PheasantTails.TwiHigh.BlazorApp.Client.Views.Components;
+
+public partial class THFollowButton : TwiHighComponentBase
+{
+    [Parameter]
+    public bool IsFollowing { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClickFollowButton { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClickRemoveButton { get; set; }
+
+    private string FollowedButtonText { get; set; } = "フォロー中";
+
+    private void OnMouseOverRemoveButton(MouseEventArgs e)
+    {
+        FollowedButtonText = "リムーブする";
+    }
+
+    private void OnMouseOutRemoveButton(MouseEventArgs e)
+    {
+        FollowedButtonText = "フォロー中";
+    }
+}
