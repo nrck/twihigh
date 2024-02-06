d6cc17a9fd1f91cddf25b39e0ec16dbcb405c718
け <challengerk.8000@gmail.com>
2024-02-07 01:07:42 +0900

    change: LocalTimelineStore properties

--- src/PheasantTails.TwiHigh.BlazorApp.Client/Models/LocalTimelineStore.cs
+++ src/PheasantTails.TwiHigh.BlazorApp.Client/Models/LocalTimelineStore.cs
@@ -3,15 +3,35 @@
 public class LocalTimelineStore
 {
     public static int MaximumTweets => 10000;
-    public DateTimeOffset Latest { get; set; }
-    public DateTimeOffset Oldest { get; set; }
+    public DateTimeOffset Latest
+    {
+        get
+        {
+            if (0 < Timeline.Count)
+            {
+                return Timeline.Max(t => t.UpdateAt);
+            }
+            return DateTimeOffset.MinValue;
+        }
+    }
+
+    public DateTimeOffset Oldest
+    {
+        get
+        {
+            if (0 < Timeline.Count)
+            {
+                return Timeline.Min(t => t.UpdateAt);
+            }
+            return DateTimeOffset.MinValue;
+        }
+    }
+
     public List<DisplayTweet> Timeline { get; set; } = [];
     public Guid UserId { get; set; }
     public LocalTimelineStore GetSaveData() => new()
     {
         UserId = UserId,
-        Latest = Latest,
-        Oldest = Oldest,
         Timeline = 0 < Timeline.Count ? Timeline.OrderByDescending(t => t.CreateAt)
             .Take(MaximumTweets)
             .ToList()
