af85bc0586f5f3266f202402c80ddcef06a11b6a
け <challengerk.8000@gmail.com>
2024-05-19 16:25:40 +0900

    move: Signup page

--- src/PheasantTails.TwiHigh.BlazorApp/Views/Pages/Signup.razor.cs
+++ src/PheasantTails.TwiHigh.BlazorApp/Views/Pages/Signup.razor.cs
@@ -2,7 +2,7 @@
 using PheasantTails.TwiHigh.BlazorApp.Client.ViewModels;
 using PheasantTails.TwiHigh.BlazorApp.Client.Views.Bases;
 
-namespace PheasantTails.TwiHigh.BlazorApp.Client.Views.Pages;
+namespace PheasantTails.TwiHigh.BlazorApp.Views.Pages;
 
 public partial class Signup : TwiHighPageBase
 {
